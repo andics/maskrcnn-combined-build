05af2e36ff5dafbf5a09601d0e75280bce0221f3
unknown <andics1243@gmail.com>
2022-12-08 13:07:40 +0100

    Added predictions counter

---
+++ dataset_processing/calculate_object_count_preds/objects/prediction_processor_obj.py
@@ -0,0 +1,68 @@
+import json, os
+import copy
+import numpy as np
+import cv2
+import math
+import torch
+import pycocotools.mask as mask_util
+
+from PIL import Image
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+from pycocotools import mask
+from itertools import groupby
+from skimage import measure
+from operator import itemgetter
+
+from maskrcnn_benchmark.config import cfg
+from maskrcnn_benchmark.data.datasets.coco import COCODataset
+from maskrcnn_benchmark.modeling.roi_heads.mask_head.inference import Masker
+
+from utils_gen import dataset_utils
+
+class predictionProcessor:
+    _DEBUGGING = False
+    _VERBOSE = True
+
+    def __init__(self, org_predictions_path, logger):
+        self.org_predictions_path = org_predictions_path
+        self.logger = logger
+
+
+    def read_predictions(self):
+        self.org_predictions_data = torch.load(self.org_predictions_path)
+        self.new_predictions_data = copy.deepcopy(self.org_predictions_data)
+
+        self.logger.log(f"Loaded PTH prediction data from: {self.org_predictions_path}")
+
+
+    def count_predictions(self):
+        #Workflow should be as follows:
+        #Check workflow.txt file in this directory
+
+        #Initiate Masker class for projecting masks to fit image size
+        #This class is used to transform the Maskrcnn-native 28x28 mask to fit the image size and look like something
+        #new_predictions_data: contains 5000 BoxLists, full of predictions (per image)
+
+        total_num_images = len(self.new_predictions_data)
+        total_num_preds_small = 0
+        total_num_preds_medium = 0
+        total_num_preds_large = 0
+        total_grand = 0
+
+        for ind, img_predictions in enumerate(self.new_predictions_data):
+            for prediction in img_predictions:
+                total_grand += 1
+                '''
+                if annotation['area'] <= 32**2:
+                    total_binary_mask_small += current_image_binary_mask_resized
+                    continue
+                elif annotation['area'] <= 96**2:
+                    total_binary_mask_medium += current_image_binary_mask_resized
+                    continue
+                else:
+                    total_binary_mask_large += current_image_binary_mask_resized
+                    continue
+                '''
+
+        return total_grand
---
+++ dataset_processing/calculate_object_count_preds/sequence_runner_count_preds.py
@@ -0,0 +1,76 @@
+import sys
+import os
+import subprocess
+from pathlib import Path
+
+try:
+    path_main = str(Path(os.path.dirname(os.path.realpath(__file__))).parents[1])
+    print(path_main)
+    sys.path.append(path_main)
+    os.chdir(path_main)
+    sys.path.remove('/workspace/object_detection')
+    print("Environmental paths updated successfully!")
+except Exception:
+    print("Tried to edit environmental paths but was unsuccessful!")
+
+from dataset_processing.calculate_object_count_preds.objects.logger_obj import Logger
+from dataset_processing.calculate_object_count_preds.utils.util_functions import Utilities_helper
+from dataset_processing.calculate_object_count_preds.objects.prediction_processor_obj import predictionProcessor
+
+import argparse
+
+class flowRunner:
+    #Some default (usually unnecessary to change) parameters
+    _LOGS_SUBDIR = "logs"
+    _LOGGER_NAME = "main_logger"
+    _ORIGINAL_PREDICTIONS_SUBDIR = "original_predictions"
+    _PROCESSED_PREDICTIONS_SAVE_SUBDIR = "filtered_predictions"
+    _OVERRIDE_PREDICTIONS = False
+
+    def __init__(self):
+        parser = argparse.ArgumentParser(description='Potential arguments for script')
+
+        parser.add_argument('-pp', '--predictions-path', nargs='?',
+                            type=str,
+                            default="/home/projects/bagon/andreyg/Projects/Variable_Resolution/Programming/maskrcnn_combined/trained_models/variable_pretrained_resnet/baseline_resnet_norm/inference",
+                            required = False,
+                            help='Path to prediction file(s) to be analyzed')
+
+        args = parser.parse_args()
+        self.main_predictions_path = args.predictions_path
+
+        self.main_file_dir = str(Path(os.path.dirname(os.path.realpath(__file__))))
+        self.objects_setup_complete = False
+
+
+    def run_all(self):
+        for pred_file in self.pth_file_paths_full:
+            pred_processor = predictionProcessor(pred_file, self.logger)
+            pred_processor.read_predictions()
+            pred_processor.count_predictions()
+
+
+    def setup_objects_and_file_structure(self):
+        self.utils_helper = Utilities_helper()
+
+        #Setting up logger file structure
+        self.logs_subdir = os.path.join(self.main_file_dir, flowRunner._LOGS_SUBDIR)
+        self.utils_helper.check_dir_and_make_if_na(self.logs_subdir)
+
+        #Setting up the logger
+        self.logger = Logger(logger_name = flowRunner._LOGGER_NAME,
+                             logs_subdir = self.logs_subdir,
+                             log_file_name = "log",
+                             utils_helper = self.utils_helper)
+        self.logger.log("Finished setting up logger object")
+        self.logger.log("Gathering all .pth files ...")
+        self.pth_file_paths_full = self.utils_helper.list_all_files_w_ext_in_a_parent_folder_recursively(self.main_predictions_path,
+                                                                                                         [".pth"])
+        self.logger.log(self.pth_file_paths_full)
+
+
+
+if __name__ == "__main__":
+    flow_runner = flowRunner()
+    flow_runner.setup_objects_and_file_structure()
+    flow_runner.run_all()
