6430f6b0cef8a3a06cfe280be5f4d3a40308bcbc
Andrey <andics1243@gmail.com>
2022-12-28 14:41:35 +0200

    Updating code

--- EXPERIMENTS/complete_crop_dataset_rec_featuremaps_var/sequence_runner_complete_crop_shift_record.py
+++ EXPERIMENTS/complete_crop_dataset_rec_featuremaps_var/sequence_runner_complete_crop_shift_record.py
@@ -45,13 +45,17 @@ class flowRunner:
                             help='The new location where the shifted dataset will be located',
                             default='/home/projects/bagon/andreyg/Projects/Variable_Resolution/Datasets/'
                                     'dataset_coco_2017_cropped_n_centered_w_featuremaps')
+        parser.add_argument('-en', '--experiment-name', nargs='?',
+                            type=str,
+                            required = False,
+                            help='The new location where the shifted dataset will be located',
+                            default='trial_5')
         parser.add_argument('-ll', '--lower-lengths', nargs='*',
                             type=float,
                             default = [0.05, 0.55],
                             required = False,
-                            help='The horizontal shifting to be performed on the image -'
-                                 ' counted from the top left corner as a ratio of the width')
-        parser.add_argument('-up', '--upper-lengths', nargs='*',
+                            help='The cropping of the image to be performed, first of the shifted then centered dataset')
+        parser.add_argument('-ul', '--upper-lengths', nargs='*',
                             type=float,
                             default = [0.30, 0.80],
                             required = False,
@@ -62,6 +66,11 @@ class flowRunner:
                             required = False,
                             help='The location of the configuration path of the model',
                             default='/home/projects/bagon/andreyg/Projects/Variable_Resolution/Programming/maskrcnn_combined/configs/R-101-FPN/variable_pretrained_resnet/variable_pretrained_resnet_baseline_resnet_norm.yaml')
+        parser.add_argument('-depth', '--depth', nargs='?',
+                            type=str,
+                            required = False,
+                            help='The location from which the tensor will be extracted: stem or layer3',
+                            default='stem')
 
 
         args = parser.parse_args()
@@ -70,6 +79,8 @@ class flowRunner:
         self.lower_lengths = args.lower_lengths
         self.upper_lengths = args.upper_lengths
         self.model_config_path = args.model_config_path
+        self.experiment_name = args.experiment_name
+        self.tensor_depth = args.depth
 
         self.objects_setup_complete = False
         self.setup_objects_and_variables()
@@ -93,15 +104,17 @@ class flowRunner:
         logging.info("Successfully setup logger!")
 
         self.new_dataset_location = os.path.join(self.new_dataset_base_location,
-                                                 "_shifted_" + str(self.lower_lengths[0])
+                                                 self.experiment_name + "_shifted_" + str(self.lower_lengths[0])
                                                  + "_" + str(self.upper_lengths[0]) + "_centered_"
-                                                 + str(self.lower_lengths[1]) + "_" + str(self.upper_lengths[1]))
+                                                 + str(self.lower_lengths[1]) + "_" + str(self.upper_lengths[1]) +
+                                                 f"_{self.tensor_depth}")
         self.utils_helper.check_dir_and_make_if_na(self.new_dataset_location)
 
         self.image_processor = imageAndPredictionProcessor(utils_helper=self.utils_helper, org_dataset_folder=self.dataset_locations,
                                                            new_dataset_folder=self.new_dataset_location, lower_lengths=self.lower_lengths,
                                                            upper_lengths= self.upper_lengths,
                                                            model_config_path=self.model_config_path,
+                                                           tensor_depth = self.tensor_depth,
                                                            default_vertical_cropping_percentage = flowRunner._DEFAULT_VERTICAL_CROPPING_PERCENTAGE)
 
 
