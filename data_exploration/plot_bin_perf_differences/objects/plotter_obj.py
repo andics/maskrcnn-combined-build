a76f67fa5e036ae4f3e25f5e76966960bad52c0d
unknown <andics1243@gmail.com>
2023-05-11 23:14:23 +0200

    Added several new types of data analoysis (marginal performance, moving average)

---
+++ data_exploration/plot_bin_perf_differences/objects/plotter_obj.py
@@ -0,0 +1,195 @@
+import seaborn
+import os
+import sys
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pathlib import Path
+from data_exploration.plot_bin_perf_differences.utils.util_functions import Utilities_helper
+
+class modelDifferencePlotter:
+    """Plot the difference in model performance across all metrics for two models
+
+        Notes
+        ----------
+        model_1_perf - model_2_perf will be taken as the difference value. Adjust setting models 1 and 2 accordingly
+
+        Parameters
+        ----------
+        """
+
+    _CSV_NAMING_PATTERN = "eval_across_bins_on_*.csv"
+    _BEGINNING_OF_CSV_METRICS_COLUMN_INDEX = 3
+    _END_OF_CSV_METRICS_COLUMN_INDEX_REVERS = -4
+
+    def __init__(self, model_one_avg_perf_csv_path, model_two_avg_perf_csv_path, \
+                 model_one_marg_perf_csv_path, model_two_marg_perf_csv_path,
+                 marg_avg_perf_diff_csv_path, perf_diff_curves_file_path,
+                 model_one_eval_folder, model_two_eval_folder, utils_helper):
+
+        self.model_one_avg_perf_csv_path = model_one_avg_perf_csv_path
+        self.model_one_marg_perf_csv_path = model_one_marg_perf_csv_path
+
+        self.model_two_avg_perf_csv_path = model_two_avg_perf_csv_path
+        self.model_two_marg_perf_csv_path = model_two_marg_perf_csv_path
+
+        self.marg_avg_perf_diff_csv_path = marg_avg_perf_diff_csv_path
+        self.perf_diff_curve_path = perf_diff_curves_file_path
+        self.model_one_eval_folder = model_one_eval_folder
+        self.model_two_eval_folder = model_two_eval_folder
+        self.utils_helper = utils_helper
+
+        self._s = modelDifferencePlotter._BEGINNING_OF_CSV_METRICS_COLUMN_INDEX
+        self._e = modelDifferencePlotter._END_OF_CSV_METRICS_COLUMN_INDEX_REVERS
+
+    def setup_objects_and_find_files(self):
+        self.csv_files_model_1 = glob.glob(os.path.join(self.model_one_eval_folder, modelDifferencePlotter._CSV_NAMING_PATTERN))
+        self.csv_files_model_2 = glob.glob(os.path.join(self.model_two_eval_folder, modelDifferencePlotter._CSV_NAMING_PATTERN))
+
+        assert len(self.csv_files_model_1) == 1 and len(self.csv_files_model_2) == 1
+        self.csv_files_model_1 = self.csv_files_model_1[0]
+        self.csv_files_model_2 = self.csv_files_model_2[0]
+
+
+    def create_avg_marg_and_diff_dataframes(self):
+        df_model_1_avg_metrics_per_bin_across_trials = self.create_average_value_dataframes(self.csv_files_model_1)
+        self.utils_helper.write_pd_dataframe_to_csv(df_model_1_avg_metrics_per_bin_across_trials,
+                                                    self.model_one_avg_perf_csv_path)
+        df_model_1_marg_gains_of_avg_df = self.create_marginal_gain_dataframe(df_model_1_avg_metrics_per_bin_across_trials)
+        self.utils_helper.write_pd_dataframe_to_csv(df_model_1_marg_gains_of_avg_df,
+                                                    self.model_one_marg_perf_csv_path)
+
+        df_model_2_avg_metrics_per_bin_across_trials = self.create_average_value_dataframes(self.csv_files_model_2)
+        self.utils_helper.write_pd_dataframe_to_csv(df_model_2_avg_metrics_per_bin_across_trials,
+                                                    self.model_one_avg_perf_csv_path)
+        df_model_2_marg_gains_of_avg_df = self.create_marginal_gain_dataframe(df_model_2_avg_metrics_per_bin_across_trials)
+        self.utils_helper.write_pd_dataframe_to_csv(df_model_2_marg_gains_of_avg_df,
+                                                    self.model_two_marg_perf_csv_path)
+
+        self.marg_perf_difference_df = self.calculate_dataframe_difference(df_model_1_marg_gains_of_avg_df,
+                                                                      df_model_2_marg_gains_of_avg_df)
+        self.utils_helper.write_pd_dataframe_to_csv(self.marg_perf_difference_df, self.marg_avg_perf_diff_csv_path)
+
+
+    def calculate_dataframe_difference(self, df_model_1: pd.DataFrame, df_model_2: pd.DataFrame) -> pd.DataFrame:
+        # create a new data frame that is a copy of df1
+        df_diff = df_model_1.copy()
+
+        # subtract df2 from df1 for columns 4 to 10
+        df_diff.iloc[:, self._s:self._e] =\
+            df_model_1.iloc[:, self._s:self._e].sub(df_model_2.iloc[:, self._s:self._e])
+
+        return df_diff
+
+
+    def create_average_value_dataframes(self, csv_file_path):
+        # Load the CSV file into a DataFrame
+        df = pd.read_csv(csv_file_path)
+
+        # Filter out non-numeric rows (excluding the header)
+        df = df[pd.to_numeric(df['lower_bin_thresh'], errors='coerce').notnull()]
+
+        # Group the rows by the first column and compute the mean of each group
+        unique_bin_ids = df.lower_bin_thresh.unique()
+
+        all_trials_avg_metrics_df = pd.DataFrame(columns = df.columns)
+        for bin_id in unique_bin_ids:
+            trial_rows_for_bin_id = df.loc[df['lower_bin_thresh'] == bin_id]
+            trial_rows_for_bin_id_metrics = \
+                trial_rows_for_bin_id.iloc[:, self._s:].apply(pd.to_numeric, errors = 'raise')
+            trial_rows_for_bin_id_metrics_avg = trial_rows_for_bin_id_metrics.mean(axis = 0)
+            row_to_append = trial_rows_for_bin_id.iloc[0].copy()
+            row_to_append.iloc[self._s:] =\
+                trial_rows_for_bin_id_metrics_avg
+
+            all_trials_avg_metrics_df = all_trials_avg_metrics_df.append(row_to_append)
+
+        print(all_trials_avg_metrics_df)
+        return all_trials_avg_metrics_df
+
+
+    def create_marginal_gain_dataframe(self, df: pd.DataFrame) -> pd.DataFrame:
+        # Select only the numeric columns
+        numeric_cols = df.iloc[:, self._s:self._e].select_dtypes(include='number').columns
+
+        # Compute the marginal changes for each column
+        marginal_changes = df[numeric_cols].diff()
+
+        # Replace the first row with zeros
+        marginal_changes.iloc[0] = 0
+        # Combine the marginal changes with the non-numeric columns
+        df.iloc[:, self._s:self._e] = marginal_changes
+
+        return df
+
+
+    def generate_results_graph_photo(self, eval_across_bins_graph_file_path, differnce_df):
+        # This function takes the generated .csv file and outputs a photo of the model performance graph
+        if os.path.exists(eval_across_bins_graph_file_path):
+            print("Graph file with eval across bins already exists!")
+            return
+        else:
+            print("Generating graph photo ...")
+
+        data = differnce_df
+        column_names_metrics = list(data.columns)[-28:-4]
+        bar_chart_columns = list(data.columns)[-4:]
+
+        # create a 7x4 grid of plots
+        fig, axs = plt.subplots(nrows=7, ncols=4, figsize=(16, 28))
+
+        # iterate over the grid of plots and plot each pair of columns
+        for i, ax in enumerate(axs.flat):
+            # extract the x and y columns for this plot
+            x_col = f'lower_bin_thresh'
+            x_data = data[x_col].values.astype(float)
+            if i < len(column_names_metrics):
+                y_col = column_names_metrics[i]
+                y_data = data[y_col].values
+
+                # compute the rolling average with window of 2
+                rolling_avg = data[y_col].rolling(window=2, min_periods=2).mean()
+
+                # plot the data on the current subplot
+                ax.plot(x_data, y_data, marker='o', linestyle='--')
+                ax.plot(x_data[1:], rolling_avg[1:], '--', linewidth=1.5, color='lightcoral', label='Moving Avg.')
+
+                # set the title to the name of the y column
+                ax.set_title(y_col)
+
+                # hide the x and y-axis labels and ticks
+                ax.set_xlabel('Bins (lower-thresh)')
+                ax.set_ylabel(f'{y_col}')
+                ax.set_title('')
+            else:
+                # plot the data on the current subplot as bar charts
+                y_col = bar_chart_columns[i - len(column_names_metrics)]
+                y_data = data[y_col].values
+                ax.bar(x_data, y_data, width=0.05)
+
+                # set the title to the name of the y column
+                ax.set_title(y_col)
+
+                # set the y-axis ticks to show the range of bar heights
+                max_height = int(np.ceil(y_data.max()))
+                min_height = int(np.floor(y_data.min()))
+                num_ticks = 5
+
+                y_ticks = np.asarray(self.utils_helper.generate_equispaced_numbers(min_height,
+                                                                                   max_height,
+                                                                                   num_ticks))
+                ax.set_yticks(y_ticks)
+
+                # hide the x-axis ticks and labels
+                ax.set_xlabel('Bins (lower-thresh)')
+                ax.set_ylabel(f'{y_col}')
+                ax.set_title('')
+
+        # adjust the layout of the subplots
+        fig.tight_layout()
+
+        # save the figure to a file
+        fig.savefig(eval_across_bins_graph_file_path, dpi=300)
+        print(f"Finished generating plot image!")
---
+++ data_exploration/plot_bin_perf_differences/perf_difference_seq_runner.py
@@ -0,0 +1,171 @@
+import seaborn
+import os
+import sys
+import argparse
+import re
+
+from pathlib import Path
+
+try:
+    path_main = str(Path(os.path.dirname(os.path.realpath(__file__))).parents[1])
+    print(path_main)
+    sys.path.append(path_main)
+    os.chdir(path_main)
+    sys.path.remove('/workspace/object_detection')
+    os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+    print("Environmental paths updated successfully!")
+except Exception:
+    print("Tried to edit environmental paths but was unsuccessful!")
+
+from data_exploration.plot_bin_perf_differences.utils.util_functions import Utilities_helper
+from data_exploration.plot_bin_perf_differences.objects.plotter_obj import modelDifferencePlotter
+
+class flowRunner:
+    #First string is model prefix, second is bin high-res area size
+    _MODEL_AVG_PERF_CSV_FILE_NAME_PATTERN = "%s_avg_pref_%s.csv"
+    _MODEL_MARG_PERF_CSV_FILE_NAME_PATTERN = "%s_marg_of_avg_pref_%s.csv"
+    _PERF_DIFF_CSV_FILE_NAME_PATTERN = "perf_diff_%s.csv"
+    _PERF_DIFF_PNG_FILE_NAME_PATTERN = "perf_diff_%s.png"
+
+    def __init__(self):
+        parser = argparse.ArgumentParser(description='Potential arguments for complete resolution-bin evaluation pipeline')
+        parser.add_argument('-mop', '--model-one-prefix', nargs='?',
+                            type=str,
+                            default = "var",
+                            required = False,
+                            help='The folder prefix which will be searched for to count model 1')
+        parser.add_argument('-mtp', '--model-two-prefix', nargs='?',
+                            type=str,
+                            default = "equiconst",
+                            required = False,
+                            help='The folder prefix which will be searched for to count model 2')
+        parser.add_argument('-ged', '--general-eval-dir', nargs='?',
+                            required=False,
+                            type = str,
+                            default="w:/bagon/andreyg/Projects/Variable_Resolution/Programming/maskrcnn_combined/EXPERIMENTS/bin_eval_per_obj_type_ann_norm_small_med/evaluations",
+                            help='The general directory in which all experiments (for all sizes) are stored')
+        parser.add_argument('-pcsd', '--plots-n-csv-store-dir', nargs='?',
+                            required=False,
+                            type = str,
+                            default="Q:/Projects/Variable_resolution/Programming/maskrcnn-combined-build/data_exploration/plot_bin_perf_differences/plots_and_csv",
+                            help='The general directory in which all perf. difference curves will be stored')
+
+        self.args = parser.parse_args()
+        self.model_one_prefix = self.args.model_one_prefix
+        self.model_two_prefix = self.args.model_two_prefix
+        self.general_eval_dir = self.args.general_eval_dir
+        self.plots_n_csv_store_dir = self.args.plots_n_csv_store_dir
+
+
+    def setup_objects_and_file_structure(self):
+        self.utils_helper = Utilities_helper()
+        self.comparison_storage_folder = os.path.join(self.plots_n_csv_store_dir,
+                                                      self.model_one_prefix + "_" + self.model_two_prefix)
+        self.utils_helper.check_dir_and_make_if_na(self.comparison_storage_folder)
+
+
+    def create_model_folders_pairs_n_storage(self):
+        # create a list to store the matching folder names
+        self.model_one_avg_perf_csv_paths = []
+        self.model_one_marg_perf_csv_paths = []
+
+        self.model_two_avg_perf_csv_paths = []
+        self.model_two_marg_perf_csv_paths = []
+
+        self.marg_avg_perf_diff_csv_paths = []
+        self.perf_diff_curves_file_paths = []
+        self.m1_m2_exp_folder_pairs = []
+
+        self.hr_area_sizes = []
+
+        # regular expression pattern to match an integer in a string
+        int_pattern = re.compile(r"(?<!\.)(?<!\d)(-?\d+)(?!\d|\.)")
+
+        # traverse the directory tree and find folders with prefixes "var" and "equiconst"
+        root, subdirs, _ = next(os.walk(self.general_eval_dir))
+        for model_one_dir_name in subdirs:
+            if model_one_dir_name.startswith(self.model_one_prefix):
+                # extract the integer value from the folder name
+                try:
+                    m1_name_number = int(int_pattern.findall(model_one_dir_name)[0])
+                except Exception as e:
+                    print(f"Folder {model_one_dir_name} name did not contain integers. Skipping...")
+                    continue
+
+                # look for a matching folder with prefix "var" and same integer value
+                for model_two_dir_name in subdirs:
+                    if model_two_dir_name.startswith(self.model_two_prefix):
+                        m2_name_number = int(int_pattern.findall(model_two_dir_name)[0])
+
+                        if m2_name_number == m1_name_number:
+                            # add tuple with the names of the two folders to the matching_folders list
+                            model_one_avg_perf_csv = os.path.join(self.comparison_storage_folder,
+                                                                  flowRunner._MODEL_AVG_PERF_CSV_FILE_NAME_PATTERN %
+                                                                  (self.model_one_prefix, str(m1_name_number)))
+                            model_one_marg_perf_csv = os.path.join(self.comparison_storage_folder,
+                                                                  flowRunner._MODEL_MARG_PERF_CSV_FILE_NAME_PATTERN %
+                                                                  (self.model_one_prefix, str(m1_name_number)))
+
+                            model_two_avg_perf_csv = os.path.join(self.comparison_storage_folder,
+                                                                  flowRunner._MODEL_AVG_PERF_CSV_FILE_NAME_PATTERN %
+                                                                  (self.model_two_prefix, str(m1_name_number)))
+                            model_two_marg_perf_csv = os.path.join(self.comparison_storage_folder,
+                                                                  flowRunner._MODEL_MARG_PERF_CSV_FILE_NAME_PATTERN %
+                                                                  (self.model_two_prefix, str(m1_name_number)))
+
+                            model_diff_avg_perf_csv = os.path.join(self.comparison_storage_folder,
+                                                                   flowRunner._PERF_DIFF_CSV_FILE_NAME_PATTERN % str(m1_name_number))
+                            model_diff_avg_perf_png = os.path.join(self.comparison_storage_folder,
+                                                                   flowRunner._PERF_DIFF_PNG_FILE_NAME_PATTERN % str(m1_name_number))
+
+                            self.model_one_avg_perf_csv_paths.append(model_one_avg_perf_csv)
+                            self.model_one_marg_perf_csv_paths.append(model_one_marg_perf_csv)
+
+                            self.model_two_avg_perf_csv_paths.append(model_two_avg_perf_csv)
+                            self.model_two_marg_perf_csv_paths.append(model_two_marg_perf_csv)
+
+                            self.marg_avg_perf_diff_csv_paths.append(model_diff_avg_perf_csv)
+                            self.perf_diff_curves_file_paths.append(model_diff_avg_perf_png)
+
+                            self.m1_m2_exp_folder_pairs.append((os.path.join(root, model_one_dir_name), os.path.join(root,
+                                                                                                                     model_two_dir_name)))
+                            self.hr_area_sizes.append(str(m1_name_number))
+
+    def run_comparison_for_all(self):
+
+        for model_one_avg_perf_csv_path, model_two_avg_perf_csv_path, \
+            model_one_marg_perf_csv_path, model_two_marg_perf_csv_path, \
+            marg_avg_perf_diff_csv_path, perf_diff_curves_file_path, \
+            m1_m2_exp_folder_pair, hr_area in zip(self.model_one_avg_perf_csv_paths,
+                                           self.model_two_avg_perf_csv_paths,
+                                           self.model_one_marg_perf_csv_paths,
+                                           self.model_two_marg_perf_csv_paths,
+                                           self.marg_avg_perf_diff_csv_paths,
+                                           self.perf_diff_curves_file_paths,
+                                           self.m1_m2_exp_folder_pairs,
+                                           self.hr_area_sizes):
+
+            print(f"Working on {hr_area} for {self.model_one_prefix}-{self.model_two_prefix}")
+            _model_difference_plotter = modelDifferencePlotter(model_one_avg_perf_csv_path = model_one_avg_perf_csv_path,
+                                                               model_two_avg_perf_csv_path = model_two_avg_perf_csv_path,
+                                                               model_one_marg_perf_csv_path = model_one_marg_perf_csv_path,
+                                                               model_two_marg_perf_csv_path = model_two_marg_perf_csv_path,
+                                                               marg_avg_perf_diff_csv_path = marg_avg_perf_diff_csv_path,
+                                                               perf_diff_curves_file_path = perf_diff_curves_file_path,
+                                                               model_one_eval_folder = m1_m2_exp_folder_pair[0],
+                                                               model_two_eval_folder = m1_m2_exp_folder_pair[1],
+                                                               utils_helper = self.utils_helper)
+            _model_difference_plotter.setup_objects_and_find_files()
+            _model_difference_plotter.create_avg_marg_and_diff_dataframes()
+            _model_difference_plotter.generate_results_graph_photo(_model_difference_plotter.perf_diff_curve_path,
+                                                                   _model_difference_plotter.marg_perf_difference_df)
+            print(f"Finished {hr_area}, moving to next (if any) ...")
+
+
+
+
+if __name__ == "__main__":
+    flow_runner = flowRunner()
+    flow_runner.setup_objects_and_file_structure()
+    flow_runner.create_model_folders_pairs_n_storage()
+    flow_runner.run_comparison_for_all()
